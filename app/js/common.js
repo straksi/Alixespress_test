c360b41a682f0fa8a4edc0e3d08ae117a4df815e
straksi <kk095@yandex.ru>
2019-02-21 19:44:11 +0300

    Chech Update Test

---
+++ app/js/common.js
@@ -0,0 +1,10 @@
+const cartWrapper = document.querySelector('.cart__wrapper'),
+    cart = document.querySelector('.cart'),
+    close = document.querySelector('.cart__close'),
+    open = document.querySelector('#cart'),
+    goodsBtn = document.querySelectorAll('.goods__btn'),
+    products = document.querySelectorAll('.goods__item'),
+    confirm = document.querySelector('.confirm'),
+    badge = document.querySelector('.nav__badge'),
+    totalCost = document.querySelector('.cart__total > span'),
+    titles = document.querySelectorAll('.gods__title');
